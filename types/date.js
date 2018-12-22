4c1fd381db1eb2bb632f12173c7e323b3c60196d
Steven Halase <steven.halase@gmail.com>
2018-12-22 15:27:43 -0700

    Added parsing of firebase timestamps

--- types/date.js
+++ types/date.js
@@ -1,19 +1,36 @@
 module.exports = class DateType {
   static validate(value) {
+    console.log(value);
+    value = typeof value.seconds !== 'undefined' && typeof value.nanoseconds !== 'undefined'
+      ? (value.seconds * 1000) + (value.nanoseconds / 1000000)
+      : value;
     return (new Date(value)).getTime() > 0;
   }
   
   static validateArray(value) {
-    return value.every(i => (new Date(i)).getTime() > 0);
+    return value.every(i => {
+      i = typeof i.seconds !== 'undefined' && typeof i.nanoseconds !== 'undefined'
+        ? (i.seconds * 1000) + (i.nanoseconds / 1000000)
+        : i;
+      (new Date(i)).getTime() > 0
+    });
   }
 
   static getValue(value) {
+    value = typeof value.seconds !== 'undefined' && typeof value.nanoseconds !== 'undefined'
+    ? (value.seconds * 1000) + (value.nanoseconds / 1000000)
+    : value;
     return value;
   }
 
   static getValueArray(value) {
     const retVal = [];
-    value.forEach(i => retVal.push(i));
+    value.forEach(i => {
+      i = typeof i.seconds !== 'undefined' && typeof i.nanoseconds !== 'undefined'
+        ? (i.seconds * 1000) + (i.nanoseconds / 1000000)
+        : i;
+      retVal.push(i)
+    });
     return retVal;
   }
 };
